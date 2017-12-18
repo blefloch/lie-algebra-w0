763591394ef15489b01c86d77bb911bfc945b57c
Bruno Le Floch <bruno@le-floch.fr>
2017-12-17 22:09:49 -0500

    Prove theorem for A_1, A_2, A_3.

--- Main.v
+++ Main.v
@@ -1,8 +1,55 @@
+Require Import Setoid.
 Require Import Bool.
 Require Import List.
 Require Import ZArith.
 Require Import Nat.
 Open Scope nat_scope.
+
+Section rewrite_helpers.
+  Definition andb_prop_iff x y : Is_true (x && y) <-> Is_true x /\ Is_true y :=
+    conj (andb_prop_elim x y) (andb_prop_intro x y).
+  Definition Is_true_iff x : Is_true x <-> x = true :=
+    conj (Is_true_eq_true x) (Is_true_eq_left x).
+  Definition Zleb_iff n m : (n <=? m)%Z = true <-> (n <= m)%Z :=
+    conj (Zle_bool_imp_le n m) (Zle_imp_le_bool n m).
+  Definition true_eq_true : true = true <-> True :=
+    conj (fun _ => I) (fun _ => eq_refl).
+  Theorem or_True_iff P : P /\ True <-> P.
+  Proof.
+    firstorder.
+  Qed.
+  Theorem list_eq_iff_hd_tl A (a : A) lambda b mu :
+    a::lambda = b::mu <-> a = b /\ lambda = mu.
+  Proof.
+    split ; intros.
+    inversion H ; firstorder.
+    destruct H as [H1 H2].
+    rewrite H1, H2 ; firstorder.
+  Qed.
+End rewrite_helpers.
+
+Hint Rewrite
+     andb_prop_iff
+     Is_true_iff
+     andb_true_iff
+     Nat.eqb_eq
+     Z.eqb_eq
+     Zleb_iff
+     true_eq_true
+     or_True_iff
+     list_eq_iff_hd_tl
+: rewritesome.
+
+Section Z_helpers.
+  Theorem thm_div_2_2 : (2 / 2 = 1)%Z.
+  Proof.
+    firstorder.
+  Qed.
+  Theorem thm_div_m2_2 : (-2 / 2 = -1)%Z.
+  Proof.
+    firstorder.
+  Qed.
+End Z_helpers.
 
 Section list_helpers.
   Variable A : Type.
@@ -54,7 +101,7 @@ Section list_helpers.
                         | b::mu' => (f a b)::(Zvec_long_map_zip f lambda' mu')
                       end
     end.
-  Definition Zvec_mul_nat m := map (Z.mul (Z_of_nat' m)).
+  Definition Zvec_mul m := map (Z.mul m).
   Definition Zvec_short_sub := Zvec_short_map_zip Z.sub.
   Definition Zvec_short_max := Zvec_short_map_zip Z.max.
   Definition Zvec_long_min := Zvec_long_map_zip Z.min.
@@ -172,9 +219,9 @@ Section lie.
     else
       match g with
         | lie_A (exist _ n _)
-          => let d := Z_of_nat' (Nat.gcd i (S n)) in
-             (constant_list _ (Z_of_nat' (S n - i) / d)%Z i)
-               ++ (constant_list _ (- Z_of_nat' i / d)%Z (S n - i))
+          => let d := Z.of_nat (Nat.gcd i (S n)) in
+             (constant_list _ (Z.of_nat (S n - i) / d)%Z i)
+               ++ (constant_list _ (- Z.of_nat i / d)%Z (S n - i))
         | lie_B (exist _ n _)
           => (constant_list _ 1%Z i) ++ (constant_list _ 0%Z (n - i))
         | lie_C (exist _ n _)
@@ -250,9 +297,9 @@ Section lie.
     else
       match g with
         | lie_A (exist _ n _)
-          => let d := Z_of_nat' (Nat.gcd i (S n)) in
-             (constant_list _ (- Z_of_nat' i / d)%Z (S n - i))
-               ++ (constant_list _ (Z_of_nat' (S n - i) / d)%Z i)
+          => let d := Z.of_nat (Nat.gcd i (S n)) in
+             (constant_list _ (- Z.of_nat i / d)%Z (S n - i))
+               ++ (constant_list _ (Z.of_nat (S n - i) / d)%Z i)
         | lie_B (exist _ n _)
           => (constant_list _ 0%Z (n - i)) ++ (constant_list _ 1%Z i)
         | lie_C (exist _ n _)
@@ -285,49 +332,52 @@ End lie.
 
 Section data.
   Definition is_exceptional_multiplier g i m :=
-    if m =? 0
-    then true
+    if (m <? 0)%Z
+    then false
     else
-      if (i =? 0) || (lie_rank g <? i)
-      then false
+      if (m =? 0) %Z
+      then true
       else
-        match g with
-          | lie_A (exist _ n _)
-            => (n =? 3)
-                 || (i =? 1)
-                 || (i =? n)
-          | lie_B (exist _ n _)
-            => (n =? 2)
-                 || (i =? 1)
-                 || (i =? 2) && (m <=? 2)
-                 || (m <=? 1)
-          | lie_C (exist _ n _)
-            => (n =? 2)
-                 || (n =? 3) && (i =? 3) && (m <=? 1)
-                 || (n =? 4) && (i =? 4) && (m <=? 2)
-                 || (i =? 1)
-                 || (i =? 2) && (m <=? 2)
-                 || (even i) && (m <=? 1)
-          | lie_D (exist _ n _)
-            => (n =? 3)
-                 || (n =? 4) && ((i =? 3) || (i =? 4))
-                 || (i =? 1)
-                 || (even n) && (((i =? 2) && (m <=? 2))
-                                   || ((even i) && (m <=? 1))
-                                   || ((1 + i =? n) && (m <=? 1)))
-          | lie_E6
-            => false
-          | lie_E7
-            => ((i =? 1) && (m <=? 2))
-                 || ((i =? 6) && (m <=? 1))
-                 || ((i =? 7) && (m <=? 1))
-          | lie_E8
-            => ((i =? 1) && (m <=? 1)) || ((i =? 8) && (m <=? 8))
-          | lie_F4
-            => ((i =? 1) || (i =? 4)) && (m <=? 2)
-          | lie_G2
-            => m <=? 2
-        end.
+        if (i =? 0) || (lie_rank g <? i)
+        then false
+        else
+          match g with
+            | lie_A (exist _ n _)
+              => (n =? 3)
+                   || (i =? 1)
+                   || (i =? n)
+            | lie_B (exist _ n _)
+              => (n =? 2)
+                   || (i =? 1)
+                   || (i =? 2) && (m <=? 2)%Z
+                   || (m <=? 1)%Z
+            | lie_C (exist _ n _)
+              => (n =? 2)
+                   || (n =? 3) && (i =? 3) && (m <=? 1)%Z
+                   || (n =? 4) && (i =? 4) && (m <=? 2)%Z
+                   || (i =? 1)
+                   || (i =? 2) && (m <=? 2)%Z
+                   || (even i) && (m <=? 1)%Z
+            | lie_D (exist _ n _)
+              => (n =? 3)
+                   || (n =? 4) && ((i =? 3) || (i =? 4))
+                   || (i =? 1)
+                   || (even n) && (((i =? 2) && (m <=? 2)%Z)
+                                     || ((even i) && (m <=? 1)%Z)
+                                     || ((1 + i =? n) && (m <=? 1)%Z))
+            | lie_E6
+              => false
+            | lie_E7
+              => ((i =? 1) && (m <=? 2)%Z)
+                   || ((i =? 6) && (m <=? 1)%Z)
+                   || ((i =? 7) && (m <=? 1)%Z)
+            | lie_E8
+              => ((i =? 1) && (m <=? 1)%Z) || ((i =? 8) && (m <=? 8)%Z)
+            | lie_F4
+              => ((i =? 1) || (i =? 4)) && (m <=? 2)%Z
+            | lie_G2
+              => (m <=? 2)%Z
+          end.
   Definition is_mixed_by_hand_revwt_alg g lambda :=
     (lie_is_radical_revwt_alg g lambda)
       && match g, lambda with
@@ -398,7 +448,7 @@ Section main.
   Definition Is_exceptional_revwt_alg g lambda :=
     exists i m,
       (Is_true (is_exceptional_multiplier g i m))
-      /\ lambda = Zvec_mul_nat m (lie_radical_fundamental_revwt_alg g i).
+      /\ lambda = Zvec_mul m (lie_radical_fundamental_revwt_alg g i).
   Inductive Is_mixed_revwt_alg : lie_algebra -> list Z -> Prop :=
   | mixed_by_hand :
       forall g lambda,
@@ -416,7 +466,180 @@ Section main.
         -> Is_mixed_revwt_alg g lambda.
 End main.
 
+
+Section lemmas.
+  Ltac show_mixed_by_ideal g lambda mu :=
+    match goal with
+      | [Hg : g = _, Hlambda : lambda = _ |- _]
+        =>
+        (right;
+         refine (mixed_by_ideal g lambda mu _) ;
+         split ;
+         [
+           refine (mixed_by_hand g mu _) ;
+           rewrite Hg ;
+           simpl ; trivial
+         |
+         rewrite Hg, Hlambda ;
+           unfold Zvec_short_sub, lie_is_dominant_revwt_alg ;
+           simpl ;
+           unfold Zvec_nondecb ;
+           simpl ;
+           autorewrite with rewritesome ;
+           omega
+        ])
+    end.
+  Ltac show_exceptional g lambda i m :=
+    match goal with
+      | [Hg : g = _, Hlambda : lambda = _ |- _]
+        =>
+        (
+          left;
+          exists i, m;
+          rewrite Hg, Hlambda;
+          unfold is_exceptional_multiplier;
+          split;
+          [
+            assert (m <? 0 = false)%Z as H4;
+            [apply Z.ltb_nlt; omega|];
+            rewrite H4;
+            destruct (m =? 0)%Z ; firstorder
+          |
+            simpl;
+            autorewrite with rewritesome;
+            (rewrite Z.div_1_r, Z.div_1_r)
+              || (rewrite thm_div_2_2, thm_div_m2_2)
+              || idtac;
+            firstorder
+          ]
+        )
+    end.
+
+  Theorem thm_radical_length :
+    forall g lambda,
+      Is_true (lie_is_radical_revwt_alg g lambda)
+      -> length lambda = lie_embedding_dim g.
+  Proof.
+    intros g lambda H.
+    unfold lie_is_radical_revwt_alg in H.
+    autorewrite with rewritesome in H.
+    firstorder.
+  Qed.
+  Theorem thm_main_A1 :
+    forall g lambda a b Hn,
+      g = (lie_A (exist (fun n : nat => n > 0) 1 Hn))
+      -> lambda = a::b::nil
+      -> Is_true (lie_is_radical_revwt_alg g lambda)
+      -> (Is_exceptional_revwt_alg g lambda) \/ (Is_mixed_revwt_alg g lambda).
+  Proof.
+    intros g lambda a b Hn Hg Hlambda.
+    intros H.
+    rewrite Hg, Hlambda in H.
+    unfold lie_is_radical_revwt_alg in H.
+    simpl in H.
+    unfold Zvec_nondecb in H.
+    simpl in H.
+    autorewrite with rewritesome in H.
+    destruct H as [Hab Hsum].
+    (*All representations of A1 are exceptional.*)
+    show_exceptional g lambda 1 b.
+  Qed.
+  Theorem thm_main_A2 :
+    forall g lambda a b c Hn,
+      g = (lie_A (exist (fun n : nat => n > 0) 2 Hn))
+      -> lambda = a::b::c::nil
+      -> Is_true (lie_is_radical_revwt_alg g lambda)
+      -> (Is_exceptional_revwt_alg g lambda) \/ (Is_mixed_revwt_alg g lambda).
+  Proof.
+    intros g lambda a b c Hn Hg Hlambda.
+    intros H.
+    rewrite Hg, Hlambda in H.
+    unfold lie_is_radical_revwt_alg in H.
+    simpl in H.
+    unfold Zvec_nondecb in H.
+    simpl in H.
+    autorewrite with rewritesome in H.
+    destruct H as [[Hab Hbc] Hsum].
+    destruct (Z_le_lt_eq_dec _ _ Hab).
+    - destruct (Z_le_lt_eq_dec _ _ Hbc).
+      + (*a<b<c implies (a,b,c) dominates (-1,0,1), mixed "by hand"*)
+        show_mixed_by_ideal g lambda (-1::0::1::nil)%Z.
+      + (*a<b=c implies (a,b,c) = c (-2,1,1) exceptional.*)
+        show_exceptional g lambda 2 c.
+    - (*a=b<=c implies (a,b,c) = -a (-1,-1,2) exceptional.*)
+      show_exceptional g lambda 1 (-a)%Z.
+  Qed.
+  Theorem thm_main_A3 :
+    forall g lambda a b c d Hn,
+      g = (lie_A (exist (fun n : nat => n > 0) 3 Hn))
+      -> lambda = a::b::c::d::nil
+      -> Is_true (lie_is_radical_revwt_alg g lambda)
+      -> (Is_exceptional_revwt_alg g lambda) \/ (Is_mixed_revwt_alg g lambda).
+  Proof.
+    intros g lambda a b c d Hn Hg Hlambda.
+    intros H.
+    rewrite Hg, Hlambda in H.
+    unfold lie_is_radical_revwt_alg in H.
+    simpl in H.
+    unfold Zvec_nondecb in H.
+    simpl in H.
+    autorewrite with rewritesome in H.
+    destruct H as [[Hab [Hbc Hcd]] Hsum].
+    destruct (Z_le_lt_eq_dec _ _ Hab).
+    - (*a<b*)
+      destruct (Z_le_lt_eq_dec _ _ Hcd).
+      + (*a<b/\c<d implies (a,b,c,d) dominates (-1,0,0,1), mixed*)
+        show_mixed_by_ideal g lambda (-1::0::0::1::nil)%Z.
+      + (*a<b/\c=d implies a<=b-2 by parity*)
+        assert (a <= b - 1)%Z as Hab1. omega.
+        assert (a <= b - 2)%Z as Hab2. destruct (Z_le_lt_eq_dec _ _ Hab1) ; omega.
+        destruct (Z_le_lt_eq_dec _ _ Hbc).
+        {
+          (*a<b<c=d implies (a,b,c,d) dominates (-2,0,1,1), mixed*)
+          show_mixed_by_ideal g lambda (-2::0::1::1::nil)%Z.
+        }
+        {
+          (*a<b=c=d implies (a,b,c,d) = b (-3,1,1,1) exceptional*)
+          show_exceptional g lambda 3 d.
+        }
+    - (*a=b*)
+      destruct (Z_le_lt_eq_dec _ _ Hbc).
+      + (*a=b<c*)
+        destruct (Z_le_lt_eq_dec _ _ Hcd).
+        * (*a=b<c<d implies (a,b,c,d) dominates (-1,-1,0,2), mixed*)
+          show_mixed_by_ideal g lambda (-1::-1::0::2::nil)%Z.
+        * (*a=b<c=d implies (a,b,c,d)=d(-1,-1,1,1) exceptional*)
+          show_exceptional g lambda 2 d.
+      + (*a=b=c<=d implies (a,b,c,d)=-a(-1,-1,-1,3) exceptional*)
+        show_exceptional g lambda 1 (-a)%Z.
+  Qed.
+End lemmas.
+
+
+
 Theorem main :
   forall g lambda,
     Is_true (lie_is_radical_revwt_alg g lambda)
     -> (Is_exceptional_revwt_alg g lambda) \/ (Is_mixed_revwt_alg g lambda).
+Proof.
+  intros g lambda Hrad.
+  pose (Hlength := thm_radical_length _ _ Hrad).
+  pose (gcopy := g).
+  pose (lambdacopy := lambda).
+  destruct g as [[n Hn]|[n Hn]|[n Hn]|[n Hn]| | | | |].
+  * (*A-type Lie algebras*)
+    simpl in Hlength.
+    clearbody Hlength.
+    destruct n as [|[|[|[|n]]]].
+  - (*A_0*)
+    omega.
+  - (*A_1*)
+    destruct lambda as [|a [|b [|]]] ; simpl in Hlength ; firstorder.
+    exact (thm_main_A1 _ _ _ _ _ (eq_refl _) (eq_refl _) Hrad).
+  - (*A_2*)
+    destruct lambda as [|a [|b [|c [|]]]] ; simpl in Hlength ; firstorder.
+    exact (thm_main_A2 _ _ _ _ _ _ (eq_refl _) (eq_refl _) Hrad).
+  - (*A_3*)
+    destruct lambda as [|a [|b [|c [|d [|]]]]] ; simpl in Hlength ; firstorder.
+    exact (thm_main_A3 _ _ _ _ _ _ _ (eq_refl _) (eq_refl _) Hrad).
+  - (*A_n, n>=4*)
