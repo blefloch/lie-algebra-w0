921ed4733513f5576cb36f8340af23760a2eef0e
Bruno Le Floch <bruno@le-floch.fr>
2017-12-21 13:14:52 -0500

    Move some list-oriented commands from Rewriting.v to ListExtras.v

--- ListExtras.v
+++ ListExtras.v
@@ -3,7 +3,47 @@ Require Import ZArith.
 Require Import Nat.
 Open Scope nat_scope.
 
+Section hdn_tln.
+  Fixpoint hdn {A} n (l : list A) :=
+    match n with
+      | 0 => nil
+      | S n' => match l with
+                  | nil => nil
+                  | a::l' => a::(hdn n' l')
+                end
+    end.
+  Fixpoint tln {A} n (l : list A) :=
+    match n with
+      | 0 => l
+      | S n' => match l with
+                  | nil => nil
+                  | a::l' => (tln n' l')
+                end
+    end.
+  Theorem thm_hdn_tln :
+    forall A n (l : list A), n <= length l -> l = (hdn n l) ++ (tln n l).
+  Proof.
+    induction n.
+    - simpl ; trivial.
+    - destruct l as [|a l].
+      + simpl ; trivial.
+      + simpl.
+        intros.
+        rewrite <- IHn.
+        * trivial.
+        * omega.
+  Qed.
+End hdn_tln.
+
 Section list_helpers.
+  Theorem list_eq_iff_hd_tl A (a : A) lambda b mu :
+    a::lambda = b::mu <-> a = b /\ lambda = mu.
+  Proof.
+    split ; intros.
+    inversion H ; firstorder.
+    destruct H as [H1 H2].
+    rewrite H1, H2 ; firstorder.
+  Qed.
   Theorem thm_last_app:
     forall A lambda a mu (b : A),
       last (lambda ++ (a::mu)) b = last (a::mu) b.
@@ -146,3 +186,85 @@ Section list_helpers.
     - simpl ; intros ; rewrite IHp ; firstorder.
   Qed.
 End list_helpers.
+
+Section rewrite_length.
+  Variable A : Type.
+  Theorem hdn_length :
+    forall n (l : list A), length (hdn n l) = min n (length l).
+  Proof.
+    induction n.
+    - simpl ; trivial.
+    - destruct l.
+      + simpl ; trivial.
+      + simpl.
+        rewrite IHn.
+        trivial.
+  Qed.
+  Theorem tln_length :
+    forall n (l : list A), length (tln n l) = length l - n.
+  Proof.
+    induction n.
+    - simpl.
+      trivial.
+      intros.
+      omega.
+    - destruct l.
+      + simpl ; trivial.
+      + simpl.
+        rewrite IHn.
+        trivial.
+  Qed.
+  Theorem thm_cons_length :
+    forall (a : A) lambda,
+      length (a::lambda) = 1 + length lambda.
+  Proof.
+    firstorder.
+  Qed.
+  Theorem thm_tl_length :
+    forall (lambda : list A),
+      length (tl lambda) = length lambda - 1.
+  Proof.
+    destruct lambda ; firstorder.
+  Qed.
+  Theorem thm_nnil_length :
+    forall (l : list A), l <> nil <-> length l > 0.
+  Proof.
+    destruct l ; simpl ; firstorder.
+  Qed.
+  Theorem thm_removelast_length :
+    forall (l : list A), length (removelast l) = length l - 1.
+  Proof.
+    destruct l as [|a l].
+    - trivial.
+    - generalize a.
+      induction l.
+      + trivial.
+      + simpl.
+        simpl in IHl.
+        rewrite IHl.
+        intros.
+        omega.
+  Qed.
+End rewrite_length.
+
+Hint Rewrite
+     thm_cons_length
+     thm_tl_length
+     thm_nnil_length
+     map_length
+     rev_length
+     app_length
+     repeat_length
+     thm_removelast_length
+     hdn_length
+     tln_length
+: rewritelength.
+
+Tactic Notation "simpl_length" :=
+  repeat (simpl || autorewrite with rewritelength).
+Tactic Notation "simpl_length" "in" hyp(H) :=
+  repeat (simpl in H || autorewrite with rewritelength in H).
+Ltac tac_length := simpl_length ; try omega.
+Ltac destruct_bool b H :=
+  destruct (Sumbool.sumbool_of_bool b) as [H|H] ;
+  rewrite H.
--- Rewriting.v
+++ Rewriting.v
@@ -4,6 +4,7 @@ Require Import List.
 Require Import ZArith.
 Require Import Nat.
 (*Load our files*)
+Require Import ListExtras.
 Open Scope nat_scope.
 
 Section rewrite_helpers.
@@ -11,22 +12,10 @@ Section rewrite_helpers.
     conj (Zle_bool_imp_le n m) (Zle_imp_le_bool n m).
   Definition true_eq_true : true = true <-> True :=
     conj (fun _ => I) (fun _ => eq_refl).
-  Theorem or_True_iff : forall P, P /\ True <-> P.
-  Proof.
-    firstorder.
-  Qed.
-  Theorem or_True_iff2 : forall P, True /\ P <-> P.
-  Proof.
-    firstorder.
-  Qed.
-  Theorem list_eq_iff_hd_tl A (a : A) lambda b mu :
-    a::lambda = b::mu <-> a = b /\ lambda = mu.
-  Proof.
-    split ; intros.
-    inversion H ; firstorder.
-    destruct H as [H1 H2].
-    rewrite H1, H2 ; firstorder.
-  Qed.
+  Definition or_True_iff P : P /\ True <-> P :=
+    conj (and_ind (fun H0 _ => H0)) (fun H => conj H I).
+  Definition or_True_iff2 P : True /\ P <-> P :=
+    conj (and_ind (fun _ H1 => H1)) (conj I).
   Definition eq_S_iff n m : S n = S m <-> n = m :=
     conj (eq_add_S n m) (f_equal_nat nat S n m).
   Definition le_S_n_iff n m : S n <= S m <-> n <= m :=
@@ -64,112 +53,3 @@ Tactic Notation "simpl_extra" "in" hyp(H) :=
   repeat (simpl in H || autorewrite with rewritesome in H).
 Tactic Notation "simpl_destruct" hyp(H) "as" simple_intropattern(pattern) :=
   simpl_extra in H ; destruct H as pattern.
-Tactic Notation "simpl_length" :=
-  repeat (simpl || autorewrite with rewritelength).
-Tactic Notation "simpl_length" "in" hyp(H) :=
-  repeat (simpl in H || autorewrite with rewritelength in H).
-
-Section rewrite_length.
-  Fixpoint hdn {A} n (l : list A) :=
-    match n with
-      | 0 => nil
-      | S n' => match l with
-                  | nil => nil
-                  | a::l' => a::(hdn n' l')
-                end
-    end.
-  Fixpoint tln {A} n (l : list A) :=
-    match n with
-      | 0 => l
-      | S n' => match l with
-                  | nil => nil
-                  | a::l' => (tln n' l')
-                end
-    end.
-  Variable A : Type.
-  Theorem thm_hdn_tln :
-    forall n (l : list A), n <= length l -> l = (hdn n l) ++ (tln n l).
-  Proof.
-    induction n.
-    - simpl ; trivial.
-    - destruct l as [|a l].
-      + simpl ; trivial.
-      + simpl.
-        intros.
-        rewrite <- IHn.
-        * trivial.
-        * omega.
-  Qed.
-  Theorem hdn_length :
-    forall n (l : list A), length (hdn n l) = min n (length l).
-  Proof.
-    induction n.
-    - simpl ; trivial.
-    - destruct l.
-      + simpl ; trivial.
-      + simpl.
-        rewrite IHn.
-        trivial.
-  Qed.
-  Theorem tln_length :
-    forall n (l : list A), length (tln n l) = length l - n.
-  Proof.
-    induction n.
-    - simpl.
-      trivial.
-      intros.
-      omega.
-    - destruct l.
-      + simpl ; trivial.
-      + simpl.
-        rewrite IHn.
-        trivial.
-  Qed.
-  Theorem thm_cons_length :
-    forall (a : A) lambda,
-      length (a::lambda) = 1 + length lambda.
-  Proof.
-    firstorder.
-  Qed.
-  Theorem thm_tl_length :
-    forall (lambda : list A),
-      length (tl lambda) = length lambda - 1.
-  Proof.
-    destruct lambda ; firstorder.
-  Qed.
-  Theorem thm_nnil_length :
-    forall (l : list A), l <> nil <-> length l > 0.
-  Proof.
-    destruct l ; simpl ; firstorder.
-  Qed.
-  Theorem thm_removelast_length :
-    forall (l : list A), length (removelast l) = length l - 1.
-  Proof.
-    destruct l as [|a l].
-    - trivial.
-    - generalize a.
-      induction l.
-      + trivial.
-      + simpl.
-        simpl in IHl.
-        rewrite IHl.
-        intros.
-        omega.
-  Qed.
-End rewrite_length.
-Hint Rewrite
-     thm_cons_length
-     thm_tl_length
-     thm_nnil_length
-     map_length
-     rev_length
-     app_length
-     repeat_length
-     thm_removelast_length
-     hdn_length
-     tln_length
-: rewritelength.
-Ltac tac_length := simpl_length ; try omega.
-Ltac destruct_bool b H :=
-  destruct (Sumbool.sumbool_of_bool b) as [H|H] ;
-  rewrite H.
