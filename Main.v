b70bfe2d46362a45ca1d4a4ae44fbe5ac9fbb8ba
Bruno Le Floch <bruno@le-floch.fr>
2017-12-22 23:45:22 -0500

    Remove dependencies in Main

--- Main.v
+++ Main.v
@@ -2,22 +2,12 @@
 
    coqc ListExtras.v Tools.v Zvec.v SimpleLieAlgebras.v Data.v TypeAll.v TypeA.v TypeBD.v TypeC.v TypeEFG.v Main.v
 *)
-Require Import Setoid.
-Require Import Bool.
-Require Import List.
-Require Import ZArith.
-Require Import Nat.
-(*Load our files*)
-Require Import Tools.
-Require Import ListExtras.
-Require Import Zvec.
 Require Import SimpleLieAlgebras.
 Require Import Data.
-Require Import TypeAll.
-Require Import TypeA.
-Require Import TypeBD.
-Require Import TypeC.
-Require Import TypeEFG.
+Require TypeA.
+Require TypeBD.
+Require TypeC.
+Require TypeEFG.
 Open Scope nat_scope.
 
 Theorem main :
@@ -26,14 +16,14 @@ Theorem main :
     -> (Is_exceptional_revwt_alg g lambda) \/ (Is_mixed_revwt_alg g lambda).
 Proof.
   intros g lambda Hrad.
-  destruct g as [[n Hn]|[n Hn]|[n Hn]|[n Hn]| | | | |].
-  - refine (thm_main_A _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_B _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_C _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_D _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_E6 _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_E7 _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_E8 _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_F4 _ _ _ Hrad) ; firstorder.
-  - refine (thm_main_G2 _ _ _ Hrad) ; firstorder.
+  destruct g.
+  - refine (TypeA.thm_main_A _ _ _ Hrad) ; firstorder.
+  - refine (TypeBD.thm_main_B _ _ _ Hrad) ; firstorder.
+  - refine (TypeC.thm_main_C _ _ _ Hrad) ; firstorder.
+  - refine (TypeBD.thm_main_D _ _ _ Hrad) ; firstorder.
+  - refine (TypeEFG.thm_main_E6 _ _ _ Hrad) ; firstorder.
+  - refine (TypeEFG.thm_main_E7 _ _ _ Hrad) ; firstorder.
+  - refine (TypeEFG.thm_main_E8 _ _ _ Hrad) ; firstorder.
+  - refine (TypeEFG.thm_main_F4 _ _ _ Hrad) ; firstorder.
+  - refine (TypeEFG.thm_main_G2 _ _ _ Hrad) ; firstorder.
 Qed.
